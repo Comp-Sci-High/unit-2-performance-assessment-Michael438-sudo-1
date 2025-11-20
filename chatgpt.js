beae6f79de64f59705236d3013ff460638606111
Michael438-sudo <michael.robinson26@compscihigh.org>
2025-11-20 13:26:51 -0500

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,27 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let requestURL = "https://api.openai.com/v1/responses"
+
+async function chatter(){
+    const options = {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + apiKey,
+            "Content-Type": 'application/json'
+
+        },
+        body: JSON.stringify({
+            model: prompt("What model are you using: "),
+            input: prompt("What would you like to ask the chatBox: ")
+        })
+
+
+    }
+    const response = await fetch(requestURL, options)
+    const data = await response.json();
+    console.log(data)
+    return data;
+}
+
+chatter(requestURL)
